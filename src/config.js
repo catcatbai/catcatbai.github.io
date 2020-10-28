f7568301760a6a404d36cf90c546a8b92f0402e9
baitao <894164233@qq.com>
2020-10-28 21:39:22 +0800

    dev, from "https://github.com/guanpengchn/guanpengchn.github.io.git" dev

---
+++ src/config.js
@@ -0,0 +1,137 @@
+export const clientId= '3bffa5a5593d3f112758'
+export const clientSecret= '1147929f2267cfcb5abb1e27e91a98a47316c557'
+export const owner = 'guanpengchn'
+
+export const nav = [
+  { 
+    text: '文章列表',
+    path: '/article'
+  },
+  { 
+    text: '学习笔记',
+    path: '/study'
+  },
+  {
+    text: '算法练习',
+    path: '/algorithm'
+  },
+  {
+    text: '收藏列表',
+    path: '/note'
+  },
+  { 
+    text: '演示列表',
+    path: '/presentation'
+  },
+  {
+    text: '关于我',
+    path: '/about'
+  },
+  {
+    text: '友情链接',
+    path: '/friend'
+  }
+]
+
+export const article = {
+  repo:'guanpengchn.github.io',
+  label:'article'
+}
+
+export const study = {
+  repo:'guanpengchn.github.io',
+  label:'study'
+}
+
+export const algorithm = {
+  repo:'guanpengchn.github.io',
+  label:'algorithm'
+}
+
+export const note = {
+  repo:'guanpengchn.github.io',
+  label:'note'
+}
+
+export const presentation = {
+  repo:'presentation',
+  filename:'README.md'
+}
+
+export const book = {
+  repo:'aaron.books'
+}
+
+export const friend = {
+  items:[
+    {
+      name: 'ylhao',
+      url: 'https://ylhao.github.io/',
+      avatar: 'https://avatars1.githubusercontent.com/u/26250897?s=100&v=4'
+    },
+    {
+      name: 'zhangyachen',
+      url: 'https://zhangyachen.github.io/',
+      avatar: 'https://avatars1.githubusercontent.com/u/14103319?s=100&v=4'
+    },
+    {
+      name: 'Chen Weihang',
+      url: 'sunny_cwh@163.com',
+      avatar: 'https://avatars1.githubusercontent.com/u/22561442?s=100&v=4'
+    },
+    {
+      name: 'zhning12',
+      url: 'https://blog.csdn.net/zhning12L',
+      avatar: 'https://avatars2.githubusercontent.com/u/28998173?s=100&v=4'
+    },
+    {
+      name: 'Monine',
+      url: 'https://monine.github.io/#/',
+      avatar: 'https://avatars2.githubusercontent.com/u/8335856?s=100&v=4'
+    },
+    {
+      name: 'Ruan YiFeng',
+      url: 'http://www.ruanyifeng.com/blog/',
+      avatar: 'https://avatars3.githubusercontent.com/u/905434?s=100&v=4'
+    },
+    {
+      name: 'Michael Liao',
+      url: 'https://www.liaoxuefeng.com/',
+      avatar: 'https://avatars0.githubusercontent.com/u/470058?s=100&v=4'
+    }
+  ],
+  libs: [
+    {
+      name: 'Vue',
+      url: 'https://github.com/vuejs/vue'
+    },
+    {
+      name: 'Vuetify',
+      url: 'https://github.com/vuetifyjs'
+    },
+    {
+      name: 'axios',
+      url: 'https://github.com/axios/axios'
+    },
+    {
+      name: 'marked',
+      url: 'https://github.com/markedjs/marked'
+    },
+    {
+      name: 'highlight.js',
+      url: 'https://github.com/highlightjs/highlight.js'
+    },
+    {
+      name: 'gulp',
+      url: 'https://github.com/gulpjs/gulp'
+    },
+    {
+      name: 'GitHub Corners',
+      url: 'https://github.com/tholman/github-corners'
+    },
+    {
+      name: 'Animated Books with CSS 3D Transforms',
+      url: 'https://tympanus.net/codrops/2013/07/11/animated-books-with-css-3d-transforms/'
+    }
+  ]
+}
